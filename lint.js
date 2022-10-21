6ee1a48dd78191ce6dc8a1b28f7d3d304cf4d8db
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2022-10-21 20:55:30 +0200

    Detects if a translation looks like English

--- lint.js
+++ lint.js
@@ -1,4 +1,8 @@
 import fs from "fs";
+import languagedetect from "languagedetect";
+const LanguageDetect = languagedetect;
+const languageDetect = new LanguageDetect();
+languageDetect.setLanguageType("iso2");
 const locales = JSON.parse(await fs.promises.readFile("cache/locales.json"));
 const keys = JSON.parse(await fs.promises.readFile("cache/keys.json"));
 const localeWarns = Object.create(null);
@@ -14,6 +18,7 @@ for (const locale of Object.keys(locales)) {
 	localeWarns[locale]["missing, but optional,"] = [];
 	localeWarns[locale].multiple = [];
 	localeWarns[locale].transparent = [];
+	localeWarns[locale]["English-looking"] = [];
 }
 for (const [key, locales] of Object.entries(keys)) {
 	keyWarns[key] = Object.create(null);
@@ -23,6 +28,7 @@ for (const [key, locales] of Object.entries(keys)) {
 	keyWarns[key]["missing, but optional,"] = [];
 	keyWarns[key].multiple = [];
 	keyWarns[key].transparent = [];
+	keyWarns[key]["English-looking"] = [];
 	const length = locales["en"].length;
 	const extraLocales = Object.keys(locales).filter((locale) => {
 		return length < 1 && locales[locale].length > 0;
@@ -40,8 +46,15 @@ for (const [key, locales] of Object.entries(keys)) {
 		return locales[locale].length > 1;
 	});
 	const transparentLocales = Object.keys(locales).filter((locale) => {
-		return locale !== "en" && locales[locale].filter((transparent) => {
-			return locales["en"].includes(transparent);
+		return locale !== "en" && locales[locale].filter((value) => {
+			return locales["en"].includes(value);
+		}).length > 0;
+	});
+	const englishLookingLocales = Object.keys(locales).filter((locale) => {
+		return locale !== "en" && locales[locale].filter((value) => {
+			const scores = Object.fromEntries(languageDetect.detect(value));
+			const englishLooking = (scores["en"] ?? Number.NEGATIVE_INFINITY) > (scores[locale] ?? Number.POSITIVE_INFINITY);
+			return englishLooking;
 		}).length > 0;
 	});
 	for (const locale of extraLocales) {
@@ -68,6 +81,10 @@ for (const [key, locales] of Object.entries(keys)) {
 	for (const locale of transparentLocales) {
 		localeWarns[locale].transparent.push(key);
 		keyWarns[key].transparent.push(locale);
+	}
+	for (const locale of englishLookingLocales) {
+		localeWarns[locale]["English-looking"].push(key);
+		keyWarns[key]["English-looking"].push(locale);
 	}
 }
 await fs.promises.mkdir("lint", {
